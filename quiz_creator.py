fbcb551884d936996a1447b5190492651d1dc503
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-04-14 23:39:28 +0800

    Create main window

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,6 +1,12 @@
 # Create a program that ask user for a question, it will also ask for 4 possible answer (a,b,c,d) and the correct answer. Write the collected data to a text file. Ask another question until the user chose to exit.
 
 # Import tkinter and create the main window
+
+from tkinter import *
+
+window = Tk()
+
+window.mainloop()
 # Edit the window title, size, icon, and background
 # Create entry widget for the question
 # Create entry widget for options

c4e9d57bb7a30960e121eaa9d4df51c690c76206
Shane Leanne Crisostomo <shaneleannecrisostomo@gmail.com>
2025-05-14 15:46:34 +0800

    pseudocode

---
+++ quiz.py
@@ -0,0 +1,12 @@
+# Create the Quiz program that read the output file of the Quiz Creator. The user will answer the randomly selected question and check if the answer is correct.
+
+# read the file
+# store the data
+# randomize the question order
+# create the main window
+# display one question and its choices
+# check user input
+# give feedback whether the answer is correct or not
+# loop through the randomized questions list
+# track score
+# display score at the end
